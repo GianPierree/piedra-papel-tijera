4f1a4b6d4b707cd8bf182887f7cb4b9c75c1bc53
GP MM <gian0291@gmail.com>
2017-08-26 18:35:16 -0500

    Se agrego un GUI para ingresar lo que seleccionamos

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
+import Tkinter, tkSimpleDialog
 import time
 from time import sleep
 import random
+root = Tkinter.Tk()
+root.withdraw()
 sus = "-"*35
 depo = ["piedra","papel","tijera"]
 while True:
-	x = raw_input("Que elijes? piedra, papel o tijera: ")
+	#x = raw_input("Que elijes? piedra, papel o tijera: ")
+	x = tkSimpleDialog.askstring("String", "Que elijes? piedra, papel o tijera:")
 	if x not in depo:
 		print("No hagas trampa!!!")
 		continue
@@ -14,7 +19,7 @@ while True:
 	sleep(0.5)
 	print(("computadora elijio {}.").format(pc))
 	if x==pc:
-		print "\nEmpate."
+ 		print "\nEmpate."
 	elif x=="piedra" and pc=="tijera":
 		print "\nGanaste"
 	elif x=="papel" and pc=="piedra":
